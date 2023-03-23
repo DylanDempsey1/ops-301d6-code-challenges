7c0134e3e54269c2ed940d3beabeca9720b42f6d
Dylan Dempsey <119841816+DylanDempsey1@users.noreply.github.com>
2023-03-22 22:22:40 -0700

    Create ops301-08.py

---
+++ ops301-08.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# Ops 301 Class 08 Code Challenge
+# Dylan Dempsey
+# 03/22/23
+# Create a Python script that includes the following:
+
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
+
+
+
+# Assign to a variable a list of ten funny string elements
+my_list = ['punny', 'giggly', 'snicker', 'chortle', 'guffaw', 'chuckle', 'teehee', 'lmao', 'rofl', 'lol']
+
+# Print the fourth element of the list
+print(my_list[3])
+
+# Print the sixth through the tenth element of the list
+print(my_list[5:])
+
+# Change the value of the seventh element to "onion"
+my_list[6] = 'onion'
+
+# Print the updated list to verify the changes
+print(my_list)
