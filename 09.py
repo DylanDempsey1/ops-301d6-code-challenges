9579edd8ce2aebef4a372ba75de83c09bb33f93e
DylanDempsey1 <drdempsey18@gmail.com>
2023-03-24 01:47:14 +0000

    09.py

---
+++ 09.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Ops Challenge 09 
+# Dylan Dempsey
+# 03/23/23
+# Create if statements using these logical conditionals below. Each statement should print information to the screen depending on if the condition is met.
+
+a = 10
+b = 20
+
+# Equals: a == b
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+
+# Not Equals: a != b
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+
+# Less than: a < b
+if a < b:
+    print("a is less than b")
+else:
+    print("a is not less than b")
+
+# Less than or equal to: a <= b
+if a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is greater than b")
+
+# Greater than: a > b
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+
+# Greater than or equal to: a >= b
+if a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("a is less than b")
+
+# If statement with logical conditional of your choice and including 'elif'
+if a == b:
+    print("a is equal to b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is greater than b")
+
+# If statement that includes both 'elif' and 'else'
+if a == b:
+    print("a is equal to b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is greater than b")
+
+# End 
+ 
+ # ChatGPT was referenced for this code. 
